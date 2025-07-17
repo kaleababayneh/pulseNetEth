3bc08a2bf56d6a9e9d7fa758a4b26ecf971dadfa
itu-itis21-gizaw21 <gizaw21@itu.edu.tr>
2025-07-17 15:14:40 +0200

    feat: add health data submissions and update stats

--- contracts/contracts/PulseToken.sol
+++ contracts/contracts/PulseToken.sol
@@ -7,18 +7,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract PulseToken is ERC20, Ownable {
     uint256 public constant REWARD_AMOUNT = 10 * 10**18; // 10 tokens per submission
     
-    // Address of the PulseNet contract that can mint rewards
     address public pulseNetContract;
     
     constructor(address initialOwner) ERC20("PulseToken", "PULSE") Ownable(initialOwner) {
-        // Mint initial supply to owner for any manual distributions
-        _mint(initialOwner, 100000 * 10**18); // 100k tokens for initial distribution
+        _mint(initialOwner, 100000 * 10**18);
     }
-    
-    /**
-     * @dev Set the PulseNet contract address (only owner)
-     * @param _pulseNetContract Address of the PulseNet contract
-     */
+
     function setPulseNetContract(address _pulseNetContract) external onlyOwner {
         pulseNetContract = _pulseNetContract;
     }
@@ -26,12 +20,7 @@ contract PulseToken is ERC20, Ownable {
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
-    
-    /**
-     * @dev Allow PulseNet contract to mint tokens for user rewards
-     * @param to Address to mint tokens to
-     * @param amount Amount of tokens to mint
-     */
+
     function mintReward(address to, uint256 amount) external {
         require(msg.sender == pulseNetContract, "Only PulseNet contract can mint rewards");
         _mint(to, amount);
